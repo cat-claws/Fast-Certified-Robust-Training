ea64daf8728a96e8c8d54aae7bb064165827064d
cat-claws <ruihan@SJgroup.uxb2jmgf02de3dtvd1ysb0bckh.ix.internal.cloudapp.net>
2023-09-28 09:17:07 +0000

    simplify models

--- models/feedforward.py
+++ models/feedforward.py
@@ -1,84 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn.parameter import Parameter
-import torchvision
-from .utils import Flatten
-import math
-import pdb
 
-import pytorchcv.model_provider
-import torchvision.models.resnet as resnet
 
-class ConvNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 1280)
-        self.fc2 = nn.Linear(1280, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-def convnet(in_ch=3, in_dim=32):
-    return ConvNet()
-
-import pytorchcv.models.resnet_cifar
-import pytorchcv.models.wrn_cifar
-
-def wresnet10(in_ch=3, in_dim=32):
-    return pytorchcv.models.wrn_cifar.get_wrn_cifar(num_classes=10, blocks=10, width_factor=4)#, model_name="wrn16_10_cifar10")
-
-def cresnet8(in_ch=3, in_dim=32):
-    return pytorchcv.models.resnet_cifar.get_resnet_cifar(num_classes=10, blocks=8, bottleneck=False)
-
-def cresnet14(in_ch=3, in_dim=32):
-    return pytorchcv.models.resnet_cifar.get_resnet_cifar(num_classes=10, blocks=14, bottleneck=False)
-
-def cresnet26(in_ch=3, in_dim=32):
-    return pytorchcv.models.resnet_cifar.get_resnet_cifar(num_classes=10, blocks=26, bottleneck=False)
-
-def cresnet(in_ch=3, in_dim=32):
-    return pytorchcv.model_provider.get_model(f"resnet20_cifar10", pretrained=False)
-
-def mnasnet0_5(in_ch=3, in_dim=32):
-    return torch.hub.load('pytorch/vision:v0.10.0', 'mnasnet0_5', num_classes = 10)
-
-def wideresnet(in_ch=3, in_dim=32):
-    return resnet.ResNet(resnet.Bottleneck, [1, 1, 1, 1], num_classes=10)
-
-def resnet10(in_ch=3, in_dim=32):
-    return resnet.ResNet(resnet.BasicBlock, [1, 1, 1, 1], num_classes=10)
- 
-def cnn_7layer(in_ch=3, in_dim=32, width=64, linear_size=512):
-    model = nn.Sequential(
-        nn.Conv2d(in_ch, width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(width, width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(width, 2 * width, 3, stride=2, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        Flatten(),
-        nn.Linear((in_dim//2) * (in_dim//2) * 2 * width, linear_size),
-        nn.ReLU(),
-        nn.Linear(linear_size,10)
-    )
-    return model
-
-def cnn_7layer_bn(in_ch=3, in_dim=32, width=64, linear_size=512):
+def cnn_7layer_bn(in_ch=3, in_dim=32, width=64, linear_size=512, num_class=10):
     model = nn.Sequential(
         nn.Conv2d(in_ch, width, 3, stride=1, padding=1),
         nn.BatchNorm2d(width),
@@ -95,31 +19,7 @@ def cnn_7layer_bn(in_ch=3, in_dim=32, width=64, linear_size=512):
         nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
         nn.BatchNorm2d(2 * width),
         nn.ReLU(),
-        Flatten(),
-        nn.Linear((in_dim//2) * (in_dim//2) * 2 * width, linear_size),
-        nn.ReLU(),
-        nn.Linear(linear_size,10)
-    )
-    return model
-
-def cnn_7layer_bn2(in_ch=3, in_dim=32, width=64, linear_size=512, num_class=10):
-    model = nn.Sequential(
-        nn.Conv2d(in_ch, width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(width),
-        nn.ReLU(),
-        nn.Conv2d(width, width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(width),
-        nn.ReLU(),
-        nn.Conv2d(width, 2 * width, 3, stride=2, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        Flatten(),
+        nn.Flatten(1),
         nn.Linear((in_dim//2) * (in_dim//2) * 2 * width, linear_size),
         nn.BatchNorm1d(linear_size),
         nn.ReLU(),
@@ -127,37 +27,11 @@ def cnn_7layer_bn2(in_ch=3, in_dim=32, width=64, linear_size=512, num_class=10):
     )
     return model
 
-def cnn(in_ch=3, in_dim=32):
-    return cnn_7layer_bn2(in_ch, in_dim)
-
 def cnn100(in_ch=3, in_dim=32):
     return cnn_7layer_bn2(in_ch, in_dim, num_class=100)
 
 def cnn_wide(in_ch=3, in_dim=32):
     return cnn_7layer_bn2(in_ch, in_dim, width=128)
-
-def cnn_7layer_imagenet(in_ch=3, in_dim=32, width=64, linear_size=512):
-    model = nn.Sequential(
-        nn.Conv2d(in_ch, width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(width),
-        nn.ReLU(),
-        nn.Conv2d(width, width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(width),
-        nn.ReLU(),
-        nn.Conv2d(width, 2 * width, 3, stride=2, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=2, padding=1),
-        nn.ReLU(),
-        Flatten(),
-        nn.Linear(32768, linear_size),
-        nn.ReLU(),
-        nn.Linear(linear_size,200)
-    )
-    return model
 
 def cnn_7layer_bn_imagenet(in_ch=3, in_dim=32, width=64, linear_size=512):
     model = nn.Sequential(
@@ -176,70 +50,10 @@ def cnn_7layer_bn_imagenet(in_ch=3, in_dim=32, width=64, linear_size=512):
         nn.Conv2d(2 * width, 2 * width, 3, stride=2, padding=1),
         nn.BatchNorm2d(2 * width),
         nn.ReLU(),
-        Flatten(),
+        nn.Flatten(1),
         nn.Linear(32768, linear_size),
         nn.BatchNorm1d(linear_size),
         nn.ReLU(),
         nn.Linear(linear_size,200)
     )
     return model
-
-def cnn_6layer(in_ch, in_dim, width=32, linear_size=256):
-    model = nn.Sequential(
-        nn.Conv2d(in_ch, width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(width, width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(width, 2 * width, 3, stride=2, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.ReLU(),
-        Flatten(),
-        nn.Linear((in_dim//2) * (in_dim//2) * 2 * width, linear_size),
-        nn.ReLU(),
-        nn.Linear(linear_size,10)
-    )
-    return model
-
-# FIXME: linear_size is smaller than other models
-def cnn_6layer_bn2(in_ch, in_dim, width=32, linear_size=256, num_class=10):
-    model = nn.Sequential(
-        nn.Conv2d(in_ch, width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(width),
-        nn.ReLU(),
-        nn.Conv2d(width, width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(width),
-        nn.ReLU(),
-        nn.Conv2d(width, 2 * width, 3, stride=2, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        nn.Conv2d(2 * width, 2 * width, 3, stride=1, padding=1),
-        nn.BatchNorm2d(2 * width),
-        nn.ReLU(),
-        Flatten(),
-        nn.Linear((in_dim//2) * (in_dim//2) * 2 * width, linear_size),
-        nn.BatchNorm1d(linear_size),
-        nn.ReLU(),
-        nn.Linear(linear_size, num_class)
-    )
-    return model
-
-"""DM-large"""
-def cnn_large(in_ch, in_dim, num_classes=10):
-    return nn.Sequential(
-        nn.Conv2d(in_ch, 64, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(64, 64, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(64, 128, 3, stride=2, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(128, 128, 3, stride=1, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(128, 128, 3, stride=1, padding=1),
-        nn.ReLU(),
-        Flatten(),
-        nn.Linear(128 * (in_dim//2) * (in_dim//2), 512),
-        nn.ReLU(),
-        nn.Linear(512, num_classes)
-    )
-    return model    
